9b4e463d3ff7549c4aa34309aa1d581e0c89b6ee
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2022-07-09 09:59:22 -0300

    Add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+}
+
 buildscript {
     repositories {
         google()
@@ -14,6 +18,8 @@ buildscript {
 }
 
 allprojects {
+
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     repositories {
         google()
