269e8db4ae5ee2b064505f482ad561f72b17b748
lucasrafagnin <lucas.rafagnin@luizalabs.com>
2022-07-09 10:17:11 -0300

    Add search activity

---
+++ app/src/main/java/com/rafagnin/tvshowcase/presentation/activity/SearchActivity.kt
@@ -0,0 +1,6 @@
+package com.rafagnin.tvshowcase.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.rafagnin.tvshowcase.R
+
+class SearchActivity : AppCompatActivity()
