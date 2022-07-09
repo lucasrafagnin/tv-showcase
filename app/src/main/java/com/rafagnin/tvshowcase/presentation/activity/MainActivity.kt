3e7dc31354da0edcaba9cd8c22e17d2d1285080a
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2022-07-09 11:53:25 -0300

    Main activity setup

--- app/src/main/java/com/rafagnin/tvshowcase/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/rafagnin/tvshowcase/presentation/activity/MainActivity.kt
@@ -1,6 +1,35 @@
 package com.rafagnin.tvshowcase.presentation.activity
 
+import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.rafagnin.tvshowcase.R
+import com.rafagnin.tvshowcase.databinding.MainActivityBinding
 
-class MainActivity : AppCompatActivity(R.layout.main_activity)
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        setNavController()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    private fun setNavController() {
+        val navHost = (supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment)
+        val bottomNavigation = binding.bottomNavigation
+
+        bottomNavigation.setupWithNavController(navHost.navController)
+    }
+}
