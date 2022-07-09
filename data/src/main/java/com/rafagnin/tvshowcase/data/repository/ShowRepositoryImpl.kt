ec890550b21925462c383a1441160165e7c57457
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2022-07-09 16:35:34 -0300

    Add repositories

---
+++ data/src/main/java/com/rafagnin/tvshowcase/data/repository/ShowRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.rafagnin.tvshowcase.data.repository
+
+import com.rafagnin.tvshowcase.data.local.LocalDataSource
+import com.rafagnin.tvshowcase.data.mapper.EpisodeToDomainMapper
+import com.rafagnin.tvshowcase.data.mapper.ShowToDomainMapper
+import com.rafagnin.tvshowcase.data.remote.RemoteDataSource
+import com.rafagnin.tvshowcase.domain.data.ShowRepository
+import com.rafagnin.tvshowcase.domain.model.EpisodeModel
+import com.rafagnin.tvshowcase.domain.model.ShowDetailModel
+import com.rafagnin.tvshowcase.domain.model.ShowModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ShowRepositoryImpl @Inject constructor(
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource,
+    private val showToDomainMapper: ShowToDomainMapper,
+    private val episodeToDomainMapper: EpisodeToDomainMapper
+) : ShowRepository {
+
+    override fun getFavorites(): Flow<List<ShowModel>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getShows(): List<ShowModel> {
+        return remoteDataSource.getShows().map { showToDomainMapper.map(it) }
+    }
+
+    override suspend fun searchShows(query: String): List<ShowModel> {
+        return remoteDataSource.search(query).map { showToDomainMapper.map(it) }
+    }
+
+    override suspend fun getShowDetail(id: Long): ShowDetailModel {
+        return remoteDataSource.getShowDetail(id).let { showToDomainMapper.mapDetail(it) }
+    }
+
+    override suspend fun getSchedule(date: String): List<EpisodeModel> {
+        return remoteDataSource.getSchedule(date).map { episodeToDomainMapper.map(it) }
+    }
+}
--- domain/src/main/java/com/rafagnin/tvshowcase/domain/data/ShowRepository.kt
+++ domain/src/main/java/com/rafagnin/tvshowcase/domain/data/ShowRepository.kt
@@ -1,5 +1,6 @@
 package com.rafagnin.tvshowcase.domain.data
 
+import com.rafagnin.tvshowcase.domain.model.EpisodeModel
 import com.rafagnin.tvshowcase.domain.model.ShowDetailModel
 import com.rafagnin.tvshowcase.domain.model.ShowModel
 import kotlinx.coroutines.flow.Flow
@@ -9,4 +10,5 @@ interface ShowRepository {
     suspend fun searchShows(query: String): List<ShowModel>
     suspend fun getShows(): List<ShowModel>
     suspend fun getShowDetail(id: Long): ShowDetailModel
+    suspend fun getSchedule(date: String): List<EpisodeModel>
 }
