49c0efb1b289846658e04870670e8e031c337384
lucasrafagnin <lucas.rafagnin@luizalabs.com>
2022-07-09 10:06:31 -0300

    Main Activity setup

---
+++ app/src/main/java/com/rafagnin/tvshowcase/presentantion/activity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rafagnin.tvshowcase.presentantion.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.rafagnin.tvshowcase.R
+
+class MainActivity : AppCompatActivity(R.layout.main_activity)
