d6e3d60cafca6150287db471786e149a5c044604
lucasrafagnin <lucas.rafagnin@luizalabs.com>
2022-07-09 12:13:29 -0300

    Adjust main topbar

--- app/src/main/java/com/rafagnin/tvshowcase/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/rafagnin/tvshowcase/presentation/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.rafagnin.tvshowcase.R
 import com.rafagnin.tvshowcase.databinding.MainActivityBinding
@@ -21,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         setNavController()
     }
 
+    override fun onBackPressed() = with(binding.bottomNavigation) {
+        if (selectedItemId == R.id.home_fragment) finish()
+        else selectedItemId = R.id.home_fragment
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
@@ -28,8 +34,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun setNavController() {
         val navHost = (supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment)
-        val bottomNavigation = binding.bottomNavigation
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.home_fragment,
+                R.id.schedule_fragment,
+                R.id.profile_fragment,
+            )
+        )
 
-        bottomNavigation.setupWithNavController(navHost.navController)
+        binding.toolbar.setupWithNavController(navHost.navController, appBarConfiguration)
+        binding.bottomNavigation.setupWithNavController(navHost.navController)
     }
 }
