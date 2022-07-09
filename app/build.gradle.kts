7b91ec8901a182217519936db5a8af8917662201
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2022-07-09 09:42:08 -0300

    Convert gradle files to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    id("com.android.application")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.rafagnin.tvshowcase"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.bundles.coroutines)
+    implementation(libs.bundles.moshi)
+    implementation(libs.bundles.navigation)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.bundles.room)
+
+    implementation(libs.appCompat)
+    implementation(libs.coil)
+    implementation(libs.hilt)
+    implementation(libs.liveData)
+    implementation(libs.material)
+    implementation(libs.paging)
+    implementation(libs.retrofitMoshi)
+    implementation(libs.viewModel)
+
+    kapt(libs.hiltCompiler)
+    kapt(libs.roomCompiler)
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.espresso)
+    androidTestImplementation(libs.junit)
+}
