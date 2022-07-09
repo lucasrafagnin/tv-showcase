677dad922be0f603aa5324578681f22191d63e46
lucasrafagnin <lucas.rafagnin@luizalabs.com>
2022-07-09 17:37:07 -0300

    Add detail param

--- app/src/main/java/com/rafagnin/tvshowcase/presentation/activity/DetailActivity.kt
+++ app/src/main/java/com/rafagnin/tvshowcase/presentation/activity/DetailActivity.kt
@@ -1,5 +1,12 @@
 package com.rafagnin.tvshowcase.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.EntryPoint
 
-class DetailActivity : AppCompatActivity()
+@EntryPoint
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val ID_EXTRA = "ID_EXTRA"
+    }
+}
