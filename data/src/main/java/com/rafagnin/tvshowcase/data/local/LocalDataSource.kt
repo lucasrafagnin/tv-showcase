b631293bb0a0b139625bfa5940ab58f40d3cee82
lucasrafagnin <lucas.rafagnin@luizalabs.com>
2022-07-09 23:21:59 -0300

    Config room database

--- data/src/main/java/com/rafagnin/tvshowcase/data/local/LocalDataSource.kt
+++ data/src/main/java/com/rafagnin/tvshowcase/data/local/LocalDataSource.kt
@@ -1,5 +1,14 @@
 package com.rafagnin.tvshowcase.data.local
 
+import com.rafagnin.tvshowcase.data.model.LocalShowModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class LocalDataSource @Inject constructor()
+class LocalDataSource @Inject constructor(
+    private val dao: ShowDao
+) {
+    fun getFavorites(): Flow<List<LocalShowModel>> = dao.getAll()
+    fun exist(id: Long) = dao.exist(id)
+    fun add(model: LocalShowModel) = dao.insert(model)
+    fun remove(model: LocalShowModel) = dao.delete(model)
+}
