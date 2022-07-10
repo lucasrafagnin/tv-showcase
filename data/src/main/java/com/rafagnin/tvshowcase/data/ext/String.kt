79ec506c3d42f26e3c46b219a9a6e807f63d1c8e
lucasrafagnin <lucas.rafagnin@luizalabs.com>
2022-07-10 16:30:48 -0300

    Improve episode screen to format texts, add icons and url

---
+++ data/src/main/java/com/rafagnin/tvshowcase/data/ext/String.kt
@@ -0,0 +1,7 @@
+package com.rafagnin.tvshowcase.data.ext
+
+import androidx.core.text.HtmlCompat
+
+fun String?.parseHtml() = this?.let {
+    HtmlCompat.fromHtml(it, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+}
