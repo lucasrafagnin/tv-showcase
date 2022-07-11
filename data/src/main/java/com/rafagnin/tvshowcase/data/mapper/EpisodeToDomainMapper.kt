fa81d6150bb7b5189d72efd6c4b6fdf9d77f213b
lucasrafagnin <lucas.rafagnin@luizalabs.com>
2022-07-11 10:15:40 -0300

    Extract date functions to DateHelper.kt

--- data/src/main/java/com/rafagnin/tvshowcase/data/mapper/EpisodeToDomainMapper.kt
+++ data/src/main/java/com/rafagnin/tvshowcase/data/mapper/EpisodeToDomainMapper.kt
@@ -3,10 +3,9 @@ package com.rafagnin.tvshowcase.data.mapper
 import com.rafagnin.tvshowcase.data.ext.parseHtml
 import com.rafagnin.tvshowcase.data.model.EpisodeJson
 import com.rafagnin.tvshowcase.data.model.ShowJson
+import com.rafagnin.tvshowcase.domain.common.DateHelper
 import com.rafagnin.tvshowcase.domain.model.EpisodeModel
 import com.rafagnin.tvshowcase.domain.model.ShowModel
-import java.text.SimpleDateFormat
-import java.util.*
 import javax.inject.Inject
 
 class EpisodeToDomainMapper @Inject constructor() {
@@ -19,7 +18,7 @@ class EpisodeToDomainMapper @Inject constructor() {
         season = json.season,
         episode = json.number,
         airdate = json.airdate,
-        hourAirdate = json.airstamp?.let { getHourAirDate(it) },
+        hourAirdate = json.airstamp?.let { DateHelper.getHourAirDate(it) },
         show = json.embedded?.show?.let { map(it) },
         runtime = json.runtime,
         site = json.url
@@ -30,11 +29,4 @@ class EpisodeToDomainMapper @Inject constructor() {
         name = json.name,
         image = json.image?.medium
     )
-
-    private fun getHourAirDate(date: String): String {
-        val currentSdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault())
-        val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
-        val currentDate = currentSdf.parse(date)
-        return sdf.format(currentDate?.time)
-    }
 }
--- domain/src/main/java/com/rafagnin/tvshowcase/domain/usecase/GetSchedule.kt
+++ domain/src/main/java/com/rafagnin/tvshowcase/domain/usecase/GetSchedule.kt
@@ -1,10 +1,9 @@
 package com.rafagnin.tvshowcase.domain.usecase
 
 import com.rafagnin.tvshowcase.domain.Resource
+import com.rafagnin.tvshowcase.domain.common.DateHelper
 import com.rafagnin.tvshowcase.domain.data.ShowRepository
 import com.rafagnin.tvshowcase.domain.model.EpisodeModel
-import java.text.SimpleDateFormat
-import java.util.*
 import javax.inject.Inject
 
 class GetSchedule @Inject constructor(
@@ -13,15 +12,9 @@ class GetSchedule @Inject constructor(
 
     suspend operator fun invoke(): Resource<List<EpisodeModel>> {
         return try {
-            Resource.Success(repository.getSchedule(getTodayDate()))
+            Resource.Success(repository.getSchedule(DateHelper.getTodayDate()))
         } catch (e: Exception) {
             Resource.Error(e.message)
         }
-    }
-
-    private fun getTodayDate(): String {
-        val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-        val calendar = Calendar.getInstance()
-        return sdf.format(calendar.timeInMillis)
     }
 }
