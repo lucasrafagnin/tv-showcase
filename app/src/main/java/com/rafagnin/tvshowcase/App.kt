5643a5780ed6cee68634c59228ce754528b5971f
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2022-07-11 01:13:54 -0300

    Set theme dark as default

--- app/src/main/java/com/rafagnin/tvshowcase/App.kt
+++ app/src/main/java/com/rafagnin/tvshowcase/App.kt
@@ -1,7 +1,18 @@
 package com.rafagnin.tvshowcase
 
 import android.app.Application
+import android.os.Build
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App: Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // To apply the dark theme when the system hasn't theme option
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+    }
+}
